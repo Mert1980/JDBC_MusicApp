0104da21fc8988e9705e17699404e9198b446288
Mert <mertdemirok80@gmail.com>
2021-02-21 18:18:08 +0100

    query artists from DB

--- src/main/java/be/intecbrussel/app/Main.java
+++ src/main/java/be/intecbrussel/app/Main.java
@@ -1,6 +1,9 @@
 package be.intecbrussel.app;
 
 import be.intecbrussel.data.Datasource;
+import be.intecbrussel.model.Artist;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,6 +11,15 @@ public class Main {
         if(!datasource.open()) {
             System.out.println("Can't open datasource");
             return;
+        }
+
+        List<Artist> artists = datasource.queryArtists();
+        if(artists == null){
+            System.out.println("No artists!");
+            return;
+        }
+        for (Artist artist:artists){
+            System.out.printf("ID = %s, Name = %s \n", artist.getId(), artist.getName());
         }
 
         datasource.close();
